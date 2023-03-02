382d2ead8b090cbbc2c5604a6838d396b29e9ac9
Ryan Campbell <campbellryan954@gmail.com>
2023-03-02 00:28:13 -0500

    Commiting changes

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,3 +1,11 @@
 function changeCenterImage() {
     console.log(1)
   }
+
+  function addImageToDiv() {
+    var img = document.createElement("img");
+    img.src = "Graphics/Eye.png";
+    var div = document.getElementById("center");
+    div.appendChild(img);
+    
+  }
