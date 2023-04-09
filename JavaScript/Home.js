84c175af83ae2d1fd223fa0fd618d0f3b46113e7
Ryan Campbell <campbellryan954@gmail.com>
2023-04-09 15:09:58 -0400

    w

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,11 +1,14 @@
 function changeCenterImage() {
-    console.log(1)
-  }
-
-  function addImageToDiv() {
+  console.log(1)
+}
+// function to add an image with src to the center div if the div is empty, otherwise rotate the image 90 degrees 
+function addImageToDiv(src,div) {
+  if (div.children.length == 0) {
     var img = document.createElement("img");
-    img.src = "Graphics/Eye.png";
-    var div = document.getElementById("center");
+    img.src = src;
     div.appendChild(img);
-    
+  } else {
+    div.children[0].style.transform = "rotate(90deg)";
   }
+}
+
